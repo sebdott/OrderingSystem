f60d268ed737ca4ff7b52487eb7366a54b0f8179
Seb <Seb>
2017-08-30 01:15:13 +0800

    Fix Log

--- GONOrderingSystems.Common/Providers/Implementation/PrometheusProvider.cs
+++ GONOrderingSystems.Common/Providers/Implementation/PrometheusProvider.cs
@@ -10,7 +10,8 @@ namespace GONOrderingSystems.Common.Providers.Implementation
         private readonly Counter _exceptionCounter = Metrics.CreateCounter(MetricCounter.ExceptionCounter, "Exception Api Request Counter");
         private readonly Counter _orderRequestCounter = Metrics.CreateCounter(MetricCounter.OrderRequestCounter, "Api Request Counter");
         private readonly Counter _failedRequestCounter = Metrics.CreateCounter(MetricCounter.FailedRequestCounter, "Failed Api Request Counter");
-        private readonly Counter _successOrderCounter = Metrics.CreateCounter(MetricCounter.SuccessCommitCounter, "Success Api Request Counter");
+        private readonly Counter _successOrderCreatedCounter = Metrics.CreateCounter(MetricCounter.SuccessOrderCreatedCounter, "Success Order Created Api Request Counter");
+        private readonly Counter _successOrderCounter = Metrics.CreateCounter(MetricCounter.SuccessCommitCounter, "Success Commit Api Request Counter");
         private readonly Counter _failedValidationCounter = Metrics.CreateCounter(MetricCounter.FailedValidationCounter, "Failed Validation Api Request Counter");
         private readonly Counter _failedCommitCounter = Metrics.CreateCounter(MetricCounter.FailedCommitCounter, "Failed Commit Api Request Counter");
 
@@ -26,6 +27,9 @@ namespace GONOrderingSystems.Common.Providers.Implementation
                     break;
                 case MetricCounter.SuccessCommitCounter:
                     _successOrderCounter.Inc();
+                    break;
+                case MetricCounter.SuccessOrderCreatedCounter:
+                    _successOrderCreatedCounter.Inc();
                     break;
                 case MetricCounter.FailedRequestCounter:
                     _failedRequestCounter.Inc();
